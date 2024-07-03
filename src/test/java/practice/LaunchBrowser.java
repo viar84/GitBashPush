955fa25b4ab704ac61e230d0e65d8c49d2d951f4
viar84 <viji.dhanabalan@gmail.com>
2024-07-03 11:48:16 +0530

    push

---
+++ src/test/java/practice/LaunchBrowser.java
@@ -0,0 +1,16 @@
+package practice;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class LaunchBrowser {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new EdgeDriver();
+
+	}
+
+}
